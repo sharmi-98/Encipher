0a769a8ada2d607729f1841a2bc853fa59a082ee
sharmi-98 <rathinamvijaya01@gmail.com>
2026-01-24 20:37:24 +0530

    Delete React3 directory

--- React3/component/Task1.js
+++
@@ -1,5 +0,0 @@
-function Task1({isLoggedIn}){
-    return(
-        <h2>{isLoggedIn?"Welcome User":"Please Login"}</h2>
-    );
-}export default Task1;
--- React3/component/Task8.js
+++
@@ -1,9 +0,0 @@
-function Task8(){
-    return(
-        <div className="container text-center mt-5">
-            <button className="btn btn-primary">
-                Bootstrap button
-            </button>
-        </div>
-    );
-}export default Task8;
